78383e422ceacdfced256008d29ead0c91926f08
Nikita <knanaprimer@gmail.com>
2021-10-05 19:04:33 +0300

    ref

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect} from 'react'
-import './App.css'
 import {AppBar, Button, CircularProgress, Container, IconButton, Toolbar, Typography} from '@material-ui/core'
 import LinearProgress from '@material-ui/core/LinearProgress'
 import {Menu} from '@material-ui/icons'
